b9a8363a99825eff0b9c536e1d32c2152e3a81bf
Jacky Pan <jacky.pan@yahoo.com>
2012-12-09 22:05:47 +0800

    first commit with just hello word

---
+++ hellocpp/src/hellocpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : hellocpp.cpp
+// Author      : Jacky Pan
+// Version     :
+// Copyright   : @Copyright 2012
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
